add2c66eba8bb0ba358f676157427714dff6da46
frytol <michal.fryc@seznam.cz>
2025-03-12 16:12:02 +0100

    Style for nav

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,47 +1,103 @@
 <template>
     <div class="w-full">
-        <!-- @FUTURE - Use v-for -->
+        <!-- @FUTURE - Use v-for and json -->
         <ul class="flex flex-wrap gap-y-3">
             <li class="w-full">
-                <router-link class="x-link w-full h-[48px] rounded-lg p-3" :to="`/moje-objednavky`">
-                    <ShoppingBagIcon class="h-5 w-5 mr-2 inline-block" />
-                    Moje objednávky
+                <router-link 
+                    class="x-button flex justify-between items-center gap-x-2 w-full h-[48px] rounded-lg p-3" 
+                    :to="`/moje-objednavky`"
+                    :class="{
+                        'accent-primary': $route.path === '/moje-objednavky',
+                        'accent-light text-dark': $route.path !== '/moje-objednavky'
+                    }"
+                >
+                    <ShoppingBagIcon class="h-5 w-5 inline-block" />
+                    <span class="flex-1 text-left">Moje objednávky</span>
+                    <ChevronRightIcon class="h-5 w-5 inline-block" />
                 </router-link>
             </li>
             <li class="w-full">
-                <router-link class="x-link w-full h-[48px] rounded-lg p-3" :to="`/faktury`">
-                    <DocumentCheckIcon class="h-5 w-5 mr-2 inline-block" />
-                    Faktury
+                <router-link 
+                    class="x-button flex justify-between items-center gap-x-2 w-full h-[48px] rounded-lg p-3" 
+                    :to="`/faktury`"
+                    :class="{
+                        'accent-primary': $route.path === '/faktury',
+                        'accent-light text-dark': $route.path !== '/faktury'
+                    }"
+                >
+                    <DocumentCheckIcon class="h-5 w-5 inline-block" />
+                    <span class="flex-1 text-left">Faktury</span>
+                    <ChevronRightIcon class="h-5 w-5 inline-block" />
                 </router-link>
             </li>
             <li class="w-full">
-                <router-link class="x-link w-full h-[48px] rounded-lg p-3" :to="`/`">
-                    <HeartIcon class="h-5 w-5 mr-2 inline-block" />
-                    Seznam přání
+                <router-link 
+                    class="x-button flex justify-between items-center gap-x-2 w-full h-[48px] rounded-lg p-3" 
+                    :to="`/`"
+                    :class="{
+                        'accent-primary': $route.path === '/',
+                        'accent-light text-dark': $route.path !== '/'
+                    }"
+                >
+                    <HeartIcon class="h-5 w-5 inline-block" />
+                    <span class="flex-1 text-left">Seznam přání</span>
+                    <ChevronRightIcon class="h-5 w-5 inline-block" />
                 </router-link>
             </li>
             <li class="w-full">
-                <router-link class="x-link w-full h-[48px] rounded-lg p-3" :to="`/`">
-                    <CircleStackIcon class="h-5 w-5 mr-2 inline-block" />
-                    Věrnostní program
+                <router-link 
+                    class="x-button flex justify-between items-center gap-x-2 w-full h-[48px] rounded-lg p-3" 
+                    :to="`/`"
+                    :class="{
+                        'accent-primary': $route.path === '/',
+                        'accent-light text-dark': $route.path !== '/'
+                    }"
+                >
+                    <CircleStackIcon class="h-5 w-5 inline-block" />
+                    <span class="flex-1 text-left">Věrnostní program</span>
+                    <ChevronRightIcon class="h-5 w-5 inline-block" />
                 </router-link>
             </li>
             <li class="w-full">
-                <router-link class="x-link w-full h-[48px] rounded-lg p-3" :to="`/`">
-                    <TagIcon class="h-5 w-5 mr-2 inline-block" />
-                    Slevové kódy
+                <router-link 
+                    class="x-button flex justify-between items-center gap-x-2 w-full h-[48px] rounded-lg p-3" 
+                    :to="`/`"
+                    :class="{
+                        'accent-primary': $route.path === '/',
+                        'accent-light text-dark': $route.path !== '/'
+                    }"
+                >
+                    <TagIcon class="h-5 w-5 inline-block" />
+                    <span class="flex-1 text-left">Slevové kódy</span>
+                    <ChevronRightIcon class="h-5 w-5 inline-block" />
                 </router-link>
             </li>
             <li class="w-full">
-                <router-link class="x-link w-full h-[48px] rounded-lg p-3" :to="`/`">
-                    <MagnifyingGlassIcon class="h-5 w-5 mr-2 inline-block" />
-                    Hlídané zboží
+                <router-link 
+                    class="x-button flex justify-between items-center gap-x-2 w-full h-[48px] rounded-lg p-3" 
+                    :to="`/`"
+                    :class="{
+                        'accent-primary': $route.path === '/',
+                        'accent-light text-dark': $route.path !== '/'
+                    }"
+                >
+                    <MagnifyingGlassIcon class="h-5 w-5 inline-block" />
+                    <span class="flex-1 text-left">Hlídané zboží</span>
+                    <ChevronRightIcon class="h-5 w-5 inline-block" />
                 </router-link>
             </li>
             <li class="w-full">
-                <router-link class="x-link w-full h-[48px] rounded-lg p-3" :to="`/nastaveni-uctu`">
-                    <Cog6ToothIcon class="h-5 w-5 mr-2 inline-block" />
-                    Nastavení účtu
+                <router-link 
+                    class="x-button flex justify-between items-center gap-x-2 w-full h-[48px] rounded-lg p-3" 
+                    :to="`/nastaveni-uctu`"
+                    :class="{
+                        'accent-primary': $route.path === '/nastaveni-uctu',
+                        'accent-light text-dark': $route.path !== '/nastaveni-uctu'
+                    }"
+                >
+                    <Cog6ToothIcon class="h-5 w-5 inline-block" />
+                    <span class="flex-1 text-left">Nastavení účtu</span>
+                    <ChevronRightIcon class="h-5 w-5 inline-block" />
                 </router-link>
             </li>
         </ul>
@@ -56,6 +112,7 @@
         CircleStackIcon, 
         TagIcon, 
         MagnifyingGlassIcon, 
-        Cog6ToothIcon 
+        Cog6ToothIcon,
+        ChevronRightIcon 
     } from '@heroicons/vue/24/outline'
 </script>
