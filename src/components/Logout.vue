9b1ad7f14a012cf049365a98b13a1aab13a42ada
frytol <michal.fryc@seznam.cz>
2025-03-12 16:21:11 +0100

    Create Logout component

--- src/components/Logout.vue
+++ src/components/Logout.vue
@@ -1,5 +1,12 @@
 <template>
-
+    <div class="w-full">
+        <router-link 
+            class="x-button muted accent-error flex gap-x-2 w-full font-normal h-[48px] rounded-lg p-3" 
+            :to="`/`"
+        >
+            <span class="flex-1">Odhlásit se</span>
+        </router-link>
+    </div>
 </template>
   
 <script setup lang="ts">
